e925c20d2667ae1431173e03c1b90eb9020adbbf
hrtsachdeva <hrtsachdeva@gmail.com>
2019-01-16 23:44:52 +0530

    2nd commit with added recipe and shoppinglist service

---
+++ app2/src/app/shared/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from '../recipes/recipe.model';
+import { EventEmitter } from '@angular/core';
+
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a Test Recipe', 'https://bit.ly/2TJIzUP') ,
+    new Recipe('Test2 Recipe', 'This is a Test2 Recipe', 'https://bit.ly/2CZ5rtG')
+  ];
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
